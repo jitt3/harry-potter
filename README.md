a2713a453dcc71e5e79dc6e4170ded70581193b4
Fabian Flores <fabianfa.88@gmail.com>
2023-05-25 07:54:22 -0600

    app full functionality

---
+++ README.md
@@ -0,0 +1,7 @@
+# React Query vs SWR
+
+DevTools yes partial  
+Selectors yes no Auto GC yes no Mutation hooks yes no offline Mut yes no
+Agnostic yes partial
+
+RQ bundle size 13kb SWR 4.3
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      backgroundImage: {
+        dark: "url('/src/assets/bg.svg')",
+        houses: "url('/src/assets/bg-houses.svg')",
+        gryffindor: "url('/src/assets/houses/gryffindor.svg')",
+        slytherin: "url('/src/assets/houses/slytherin.svg')",
+        hufflepuff: "url('/src/assets/houses/hufflepuff.svg')",
+        ravenclaw: "url('/src/assets/houses/ravenclaw.svg')",
+      },
+    },
+  },
+  plugins: [],
+}
